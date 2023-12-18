87f5002fec945812836748b4c406884488821189
TleWatsa <nantuko_titan@hotmail.com>
2023-12-18 14:20:33 +0700

    ci: returnString

---
+++ codewars/return_string.test.ts
@@ -0,0 +1,12 @@
+import { test , expect } from 'bun:test'
+import { greet } from './return_strings'
+
+test ('shound get "Hello, Shingles how are you doing today?"', function () {
+expect(greet("Hello, <name> how are you doing today?")).toEqual("Hello, Shingles how are you doing today?");});
+
+
+test ('shound get "Hello, Pop how are you doing today?"', function () {
+    
+    expect(greet("Hello, <name> how are you doing today?")).toEqual("Hello, Pop how are you doing today?");});
+
+
---
+++ codewars/return_strings.ts
@@ -0,0 +1,4 @@
+export function greet(name: string): string {
+    const greet = (`Hello, ${name} how are you doing today?`);
+    return greet;
+  }
