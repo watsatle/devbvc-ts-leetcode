8781b32ccaf943df844d463cd6429a03f6d3ed80
TleWatsa <nantuko_titan@hotmail.com>
2023-12-19 21:25:52 +0700

    feat: feat/codewars/createPhoneNumber

---
+++ codewars/create_phone_number.ts
@@ -0,0 +1,11 @@
+export function createPhoneNumber(numbers: number[]): string {
+
+    const firstset = numbers.slice(0,3).join('');
+   
+    const secset = numbers.slice(3,6).join('');
+   
+    const lastset = numbers.slice(-4).join('');
+  
+    
+    return "(" + firstset + ")" + " " + secset + "-" + lastset;
+  }
